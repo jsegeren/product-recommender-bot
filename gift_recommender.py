ae8fd73c5203563001ac1c02caa36d1141ea2802
jsegeren <2702674+jsegeren@users.noreply.github.com>
2023-04-22 00:37:53 -0400

    Working code - uses GPT for embeddings and completions, Pinecone for index / vector DB

--- gift_recommender.py
+++ gift_recommender.py
@@ -1,7 +1,6 @@
 import os
 import csv
 import pandas as pd
-from sentence_transformers import SentenceTransformer
 from dotenv import load_dotenv
 import openai
 import pinecone
@@ -11,54 +10,106 @@ PINECONE_API_KEY = "ddad3c74-f42d-4dfb-a88e-dd199b611d28"
 PINECONE_API_ENVIRONMENT = "northamerica-northeast1-gcp"
 OPENAI_API_KEY = "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"
 
-def preprocess_and_build_index(embeddingModel, dataframe):
+NUMBER_TOTAL_PRODUCTS = 2000
+
+
+def clear_index_data(pinecone_index):
+    # Delete all vectors from the index
+    pinecone_index.delete(deleteAll=True)
+
+
+def generate_embeddings(texts, model="text-embedding-ada-002"):
+    openai.api_key = OPENAI_API_KEY
+
+    embeddings = []
+    for text in texts:
+        response = openai.Embedding.create(input=text, engine=model)
+        embedding = response["data"][0][
+            "embedding"
+        ]  # Access the 'embedding' attribute from the 'data' attribute
+        # print("Embedding: ")
+        # print(embedding)
+        embeddings.append(embedding)
+
+    return embeddings
+
+
+def preprocess_and_build_index(dataframe):
     index_name = "gift-recommender"
 
+    print("Initializing Pinecone...")
     pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_API_ENVIRONMENT)
-    # Calculate embeddings
-    dataframe["embedding"] = dataframe["product_description"].apply(lambda x: embeddingModel.encode(x))
-    embeddings = dataframe["embedding"].tolist()
+
+    # Process product descriptions and obtain embeddings
+    print("Generating embeddings...")
+    product_descriptions = dataframe["product_description"].tolist()
+    embeddings = generate_embeddings(product_descriptions)
+
+    # Get the dimension of the first embedding
+    embedding_dimension = len(embeddings[0])
+
+    # Delete existing index if it exists
+    # if index_name in pinecone.list_indexes():
+    #     pinecone.delete_index(index_name)
 
     # Check if index exists
     if index_name not in pinecone.list_indexes():
-        # Create Pinecone index
-        pinecone.create_index(name=index_name, dimension=len(embeddings[0]), metric='cosine', shards=1)
+        pinecone.create_index(
+            name=index_name, dimension=embedding_dimension, metric="cosine", shards=1
+        )
 
-        # Upsert embeddings
-        pinecone_index = pinecone.Index(index_name=index_name)
-        pinecone_index.upsert(vectors={str(idx): emb for idx, emb in enumerate(embeddings)})
-    else:
-        # Initialize the Pinecone index object
-        pinecone_index = pinecone.Index(index_name=index_name)
+    # Initialize the Pinecone index object
+    pinecone_index = pinecone.Index(index_name=index_name)
+
+    # Clear the existing data in the Pinecone index
+    print("Clearing index data...")
+    clear_index_data(pinecone_index)
+
+    # Upsert embeddings
+    print("Upserting embeddings...")
+    ids = [str(idx) for idx in range(len(product_descriptions))]
+    pinecone_index.upsert(vectors=zip(ids, embeddings))
 
     return pinecone_index
 
-def retrieve_top_products(user_description, pinecone_index, model, dataframe, top_n):
-    # Calculate the user description embedding
-    query_embedding = model.encode(user_description).tolist()  # Convert numpy array to list
+
+def retrieve_top_products(user_description, pinecone_index, dataframe, top_n):
+    # Convert user_description to an embedding
+    user_embedding = generate_embeddings([user_description])[0]
 
     # Fetch top-n nearest neighbors from Pinecone index
-    nearest_neighbors = pinecone_index.query([query_embedding], top_k=top_n, include_metadata=True)
-    
+    print("Retrieving top products...")
+    nearest_neighbors = pinecone_index.query(
+        vector=user_embedding, top_k=top_n, include_values=True
+    )
+
     # Extract product information from nearest_neighbors
     product_info = []
-    for match in nearest_neighbors['matches']:
-        product_id = int(match['id'])
+    for match in nearest_neighbors["matches"]:
+        product_id = int(match["id"])
         product = dataframe.loc[product_id]
-        product_info.append((product_id, product['product_name'], product['product_description']))
-    
+        product_info.append(
+            (product_id, product["product_name"], product["product_description"])
+        )
+
     return product_info
 
 
-def recommend_gifts(user_description, pinecone_index, model, dataframe, top_n):
-    top_products = retrieve_top_products(user_description, pinecone_index, model, dataframe, top_n)
-    
-    prompt = f"Given the shopper's request/description '{user_description}', list the top 3 gifts from our catalogue of available products:\n\n"
+def recommend_gifts(user_description, pinecone_index, dataframe, top_n):
+    top_products = retrieve_top_products(
+        user_description, pinecone_index, dataframe, top_n
+    )
 
-    for index, (product_id, product_name, product_description) in enumerate(top_products):
+    prompt = f"Given the shopper's request/description '{user_description}', choose the best gifts (min 2, max 3) from the following {top_n} options in our catalog:\n\n"
+
+    for index, (product_id, product_name, product_description) in enumerate(
+        top_products
+    ):
         prompt += f"{index + 1}. {product_name} - {product_description}\n"
 
-    prompt += "\Suggested gifts:"
+    prompt += "\nThe best gifts are:"
+
+    print("Prompt: ", prompt)
 
     openai.api_key = OPENAI_API_KEY
     response = openai.ChatCompletion.create(
@@ -70,12 +121,15 @@ def recommend_gifts(user_description, pinecone_index, model, dataframe, top_n):
         temperature=0.3,
     )
 
-    return response.choices[0].message['content'].strip()
+    return response.choices[0].message["content"].strip()
+
 
 # Generate sample data
 def generate_sample_data(filename, num_samples):
     openai.api_key = OPENAI_API_KEY
-    prompt = f"Generate {num_samples} realistic gift product names and their descriptions:\n"
+    prompt = (
+        f"Generate {num_samples} realistic gift product names and their descriptions:\n"
+    )
 
     response = openai.ChatCompletion.create(
         model="gpt-4",
@@ -86,49 +140,56 @@ def generate_sample_data(filename, num_samples):
         temperature=0.8,
     )
 
-    product_data = response.choices[0].message.content.strip().split('\n')
+    product_data = response.choices[0].message.content.strip().split("\n")
 
-    with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
-        fieldnames = ['product_name', 'product_description']
+    with open(filename, "w", newline="", encoding="utf-8") as csvfile:
+        fieldnames = ["product_name", "product_description"]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for item in product_data:
-            if ':' not in item:
+            if ":" not in item:
                 continue
 
-            product_name, product_description = item.split(':', 1)
-            writer.writerow({'product_name': product_name.strip(), 'product_description': product_description.strip()})
+            product_name, product_description = item.split(":", 1)
+            writer.writerow(
+                {
+                    "product_name": product_name.strip(),
+                    "product_description": product_description.strip(),
+                }
+            )
 
 
 # Load data
 def load_data(filename):
     data = pd.read_csv(filename)
-    data['combined_info'] = data.apply(
-        lambda x: f"{x['product_name']} {x['product_description']}",
-        axis=1
+    data["combined_info"] = data.apply(
+        lambda x: f"{x['product_name']} {x['product_description']}", axis=1
     )
     return data
 
+
 def main():
-    data_filename = 'sample_products.csv'
-    
+    data_filename = "sample_products.csv"
+
     if not os.path.exists(data_filename) or os.path.getsize(data_filename) == 0:
         print("Generating sample data...")
-        generate_sample_data(data_filename, 5)
+        generate_sample_data(data_filename, NUMBER_TOTAL_PRODUCTS)
     else:
         print("Sample data file already exists, skipping data generation.")
 
     data = load_data(data_filename)
 
-    # Initialize the sentence embedding model
-    model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
-
     # Build Pinecone index
-    pinecone_index = preprocess_and_build_index(model, data)
+    pinecone_index = preprocess_and_build_index(data)
 
     # Test Pinecone-based recommendation
-    user_description = input("Hi there! Welcome to Giftem.Shop. What kind of gift are you looking for today?\n")
-    recommendations = recommend_gifts(user_description, pinecone_index, model, data, 3)
+    user_description = input(
+        "Hi there! Welcome to Giftem.Shop. What kind of gift are you looking for today?\n"
+    )
+    recommendations = recommend_gifts(
+        user_description, pinecone_index, data, NUMBER_TOTAL_PRODUCTS
+    )
+
     print(recommendations)
 
 
